ced3efa94a3061c6439fd23378400949232298a5
JoseS19 <71463126+JoseS19@users.noreply.github.com>
2021-06-14 04:49:37 -0600

    Add files via upload

---
+++ UsuariosApi/UsuariosApi/Controllers/UsuariosController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosApi.Context;
+using UsuariosApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UsuariosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private AppDbContext _context;
+        
+        public UsuariosController(AppDbContext context) {
+            _context = context;
+        }
+
+        //Todos los usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            return await _context.Usuarios
+                .Select(x => x)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return usuario;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUsuario(int id, Usuario usuario)
+        {
+            if (id != usuario.id_usuario)
+            {
+                return BadRequest();
+            }
+
+            var usuarioM = await _context.Usuarios.FindAsync(id);
+            if (usuarioM == null)
+            {
+                return NotFound();
+            }
+
+            
+            usuarioM.nombre = usuario.nombre;
+            usuarioM.num_tel = usuario.num_tel;
+            usuarioM.pass = usuario.pass;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!UsuarioExists(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> CreateUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsuarios), usuario );
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private bool UsuarioExists(int id) =>
+         _context.Usuarios.Any(e => e.id_usuario == id);
+    }
+}
---
+++ UsuariosApi/UsuariosApi/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuariosApi.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int id_usuario { get; set; }
+        [Required]
+        public string nombre { get; set; }
+        [Required]
+        public string num_tel { get; set; }
+        [Required]
+        public string pass { get; set; }
+    }
+}
